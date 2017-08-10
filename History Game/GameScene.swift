ac3b8e15b71a7e5b9b862af11bfef1b783f9bfec
iD Student <idstudent@stratton-thirty-three.mit.edu>
2017-08-10 17:33:18 -0400

    Answer boxes nearly fixed

--- History Game/GameScene.swift
+++ History Game/GameScene.swift
@@ -13,6 +13,12 @@ class GameScene: SKScene {
     
     var cardWidth : CGFloat?
     var cardHeight : CGFloat?
+    var gameTitle = SKLabelNode(fontNamed: "Arial")
+    var creatorTitle = SKLabelNode(fontNamed: "Arial")
+    var inventionTitle = SKLabelNode(fontNamed: "Arial")
+    var countryTitle = SKLabelNode(fontNamed: "Arial")
+    var rulerTitle = SKLabelNode(fontNamed: "Arial")
+    var yearTitle = SKLabelNode(fontNamed: "Arial")
     
     var vc : GameViewController!
     weak var scrollView: SwiftySKScrollView?
@@ -20,10 +26,81 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        backgroundColor = UIColor.white
         cardWidth = (size.width) / 5
         cardHeight = (size.height) / 3
         
         addChild(moveableNode)
+        
+        //Title of the game
+        gameTitle.fontColor = UIColor.red
+        
+        gameTitle.fontSize = 30
+        
+        gameTitle.position = CGPoint(x: self.size.width * 0.1, y: self.size.height * 0.9)
+        
+        addChild(gameTitle)
+        
+        gameTitle.text = "World's greatest inventions in 17th-20th centuries"
+        
+        //Creator
+        creatorTitle.fontColor = UIColor.black
+        
+        creatorTitle.fontSize = 20
+        
+        creatorTitle.position = CGPoint(x: self.size.width * 0.1, y: self.size.height * 0.7)
+        
+        addChild(creatorTitle)
+        
+        creatorTitle.text = "Creator"
+        
+        //Invention
+        creatorTitle.fontColor = UIColor.black
+        
+        inventionTitle.fontSize = 20
+        
+        inventionTitle.position = CGPoint(x: self.size.width * 0.3, y: self.size.height * 0.7)
+        
+        addChild(inventionTitle)
+        
+        inventionTitle.text = "Invention"
+        
+        //Country
+        countryTitle.fontColor = UIColor.black
+        
+        countryTitle.fontSize = 20
+        
+        countryTitle.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.7)
+        
+        addChild(countryTitle)
+        
+        countryTitle.text = "Country"
+        
+        
+        //Ruler
+        rulerTitle.fontColor = UIColor.black
+        
+        rulerTitle.fontSize = 20
+        
+        rulerTitle.position = CGPoint(x: self.size.width * 0.7, y: self.size.height * 0.7)
+        
+        addChild(rulerTitle)
+        
+        rulerTitle.text = "Ruler"
+        
+        //Year
+        yearTitle.fontColor = UIColor.black
+        
+        yearTitle.fontSize = 20
+        
+        yearTitle.position = CGPoint(x: self.size.width * 0.9, y: self.size.height * 0.7)
+        
+        addChild(yearTitle)
+        
+        yearTitle.text = "Year"
+        
+        
+        
         
         scrollView = SwiftySKScrollView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height), moveableNode: moveableNode, direction: .vertical)
         scrollView?.contentSize = CGSize(width: scrollView!.frame.width, height: CGFloat(cards[0].count) * cardHeight!)
@@ -35,12 +112,22 @@ class GameScene: SKScene {
         
         
         for row in 0 ..< cards.count{
-            for col in 0 ..< cards[0].count{
+            for col in 0 ..< cards[row].count{
                 cards[row][col].size = CGSize(width: cardWidth!, height: cardHeight!)
-                cards[row][col].position = CGPoint(x: CGFloat(col) * cardWidth! + cardWidth!/2, y: -CGFloat(row) * cardHeight! - cardHeight!/2)
+                cards[row][col].position = CGPoint(x: CGFloat(row) * cardWidth! + cardWidth!/2, y: -CGFloat(col) * cardHeight! - cardHeight!/2 - 125)
                 moveableNode.addChild(cards[row][col])
+                var rectangle : CGRect = CGRect(x: CGFloat(row) * cardWidth! / 2, y: CGFloat(col) * cardHeight! / 2, width: 20, height: 20)
+                cards[row][col].field.frame = rectangle
+                cards[row][col].field.borderStyle = UITextBorderStyle.roundedRect
+                cards[row][col].field.textColor = UIColor.black
+                cards[row][col].field.font = UIFont(name: "helvetica", size: 8)
+                cards[row][col].color = UIColor.black
+                self.view?.addSubview(cards[row][col].field)
             }
         }
+        
+        
+        
         
         // Scroll View Stuff
         
@@ -64,5 +151,11 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        for row in 0 ..< cards.count{
+            for col in 0 ..< cards[row].count{
+                var rectangle : CGRect = CGRect(x: CGFloat(row) * cardWidth! / 2 + cardWidth!, y: (CGFloat(col) * cardHeight!) / 2 - ((scrollView?.contentOffset.y)! / 2), width: 20, height: 20)
+                cards[row][col].field.frame = rectangle
+            }
+        }
     }
 }
